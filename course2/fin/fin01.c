133107ee2902be3e49347c7c1b6c04e6a466f35c
xmu310 <xmu310@gmail.com>
2022-06-04 14:42:35 +0800

    some changes

--- course2/fin/fin01.c
+++ course2/fin/fin01.c
@@ -1,21 +1,22 @@
 #include<stdio.h>
 #include<stdint.h>
+#include<string.h>
 #include<unistd.h>
 #include<getopt.h>
-int main(int argc,char *argv){
+int main(int argc,char *argv[]){
+	int64_t x=0,y=0,tmp[3]={0},w=1024,h=768;
+	int help=0;
 	struct option optl[]={
-		{"help",0,&h,1}
+		{"help",0,&help,1},
 		{0,0,0,0}
 	};
-	char buf[1000]={0},*str[4]={"top left","top right","bottom left","bottom right"},head[54]={0},c,cc;
-	int64_t x=0,y=0,tmp[3]={0},w=1024,h=768;
+	char buf[1000]="output.bmp",*str[4]={"top left","top right","bottom left","bottom right"},head[54]={0},c,cc;
 	uint8_t color[5][3];
-	head[0x0]='B',head[0x1]='M';
 	while((c=getopt_long(argc,argv,"w:h:o:",optl,0))!=-1){
 		if(c=='w'){
-			if(sscanf(optarg,"%d%c",&w,&cc)!=1||w<0)printf("width should be a positive number\n");
+			if(sscanf(optarg,"%ld%c",&w,&cc)!=1||w<0){printf("width should be a positive number\n");return 0;}
 		}else if(c=='h'){
-			if(sscanf(optarg,"%d%c",&h,&cc)!=1||h<0)printf("width should be a positive number\n");
+			if(sscanf(optarg,"%ld%c",&h,&cc)!=1||h<0){printf("width should be a positive number\n");return 0;}
 		}else if(c=='o'){
 			strcpy(buf,optarg);
 		}else if(c!=0){
@@ -23,6 +24,17 @@ int main(int argc,char *argv){
 			return 0;
 		}
 	}
+	if(argc-optind!=0){printf("Syntax error!\n");return 0;}
+	if(help){
+		printf("Usage:\n");
+		printf("  ./fin01 [options]\n");
+		printf("    -w: the width of the output figure. (Default: 1024)\n");
+		printf("    -h: the height of the output figure. (Default: 768)\n");
+		printf("    -o: the output file name. (Default: output.bmp)\n");
+		printf("    --help: this message\n");
+		return 0;
+	}
+	head[0x0]='B',head[0x1]='M';
 	*(uint32_t*)&head[0x2]=w*h*3+54;
 	*(uint32_t*)&head[0xA]=54;
 	*(uint32_t*)&head[0xE]=40;
